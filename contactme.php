dcc2f99871acbbed9039263d7293d8cec7145806
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2020-07-30 17:59:52 -0700

    Update contactme.php page
    Include php code from test.php

--- contactme.php
+++ contactme.php
@@ -1,7 +1,24 @@
 <?php include "includes/header.php";?>
 
-<h3>Here is contact-me page</h3>
+<h1>Contact Gift</h1>
+<?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
 
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "tanakan.gth@gmail.com";  //place your/your client's email address here
+        $toName = "Gift Homsaen"; //place your client's name here
+        $website = "Contact Form Test";  //place NAME of your client's website
+
+        echo loadContact('simple.php');#demonstrates a simple contact form
+        //echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
  
 <?php include "includes/footer.php";?>
 
