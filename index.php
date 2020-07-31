2062f00c933120e0ed01a36847c083d6e62e5339
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2020-07-30 18:11:30 -0700

    This is my portal page

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php include "includes/header.php";?>
 
-<h3>Here is final project page</h3>
+<h3>Here is my why</h3>
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="Gift-Tanakan" />
+ <img src="images/phone.jpg" class="phone" alt="Gift-tanakan" />
+    
+ <p>I am a girl from a farm home in Thailand. The farm made me deeply appreciate nature and love independence. I came to the US as a student in 2018, studying University of Washington’s Global Business Program. Seattle's culture broadened my vision in powerful but contradictory ways.</p> 
+    <p>On one hand, I saw companies get richer, and people become wealthier. On the other, I saw nature being destroyed for mindless consumption, driven by marketing by those same companies. I was devastated. That harsh reality brought me more clarity; I wanted to join a company that aims to lessen our environmental impact. So, I joined Scoop Marketplace - Seattle's zero-waste grocery store as a web editor and salesperson.</p>
+ 
+</section>
+<!-- END LEFT COL -->
 
+<!-- START RIGHT COL -->
+<aside>
+ <h3 >Here is my way</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="Gift at farmer's market" />
+ <p>Working at Scoop helped me discover my joy in creating websites and want to develop a digital tool that simplifies zero-waste bulk shopping process; I decided to enroll in Programming Associate Degree at Seattle Central College. I hope to gain solid fundamental skills in programming, web design, building application. Also, I want to work with companies like, <a href="https://www.ecosia.org/">Ecosia</a>, the search engine that plants trees or <a href="https://packagefreeshop.com/">Package Free Shop</a>, the shop that sells alternative products with minimal impact. I love what they do.</p> <p>Although capitalism gives us power and freedom to consume, it also results in massive waste problems and global warming that has now gone too far...to the point of no return. <strong>I will feel like a responsible human being if I can contribute these powerful technical skills to the field that saves the world rather than destroying it.</strong></p> 
+ 
+</aside>
+<!-- END RIGHT COL -->
  
 <?php include "includes/footer.php";?>
 
